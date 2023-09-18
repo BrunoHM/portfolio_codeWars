bfbf9790b38fa812657f4351825cb74f1c658b86
Bruno Murta <brunoh.murta@gmail.com>
2023-09-18 04:31:42 -0300

    Resolution of Kata 514b92a657cdc65150000006 at https://www.codewars.com

---
+++ src/main/Kyu_6/MultiplesOf3or5.java
@@ -0,0 +1,27 @@
+package main.Kyu_6;
+
+public class MultiplesOf3or5 {
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+
+    public static int solution(int givenNumber) {
+        int answer = 0;
+        int fiveForValue = 0;
+
+        if(givenNumber > 3){
+            for(int index = 1; index <= givenNumber/3; index++){
+                fiveForValue = 5*index;
+
+                answer += 3*index;
+                if(fiveForValue < givenNumber && fiveForValue%3 != 0){
+                    answer += fiveForValue;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
+
