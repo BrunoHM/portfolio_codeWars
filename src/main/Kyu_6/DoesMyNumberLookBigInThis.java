c1bf1b73890f5bb06412e08a2978cb92e5625077
Bruno Murta <brunoh.murta@gmail.com>
2023-10-03 02:46:48 -0300

    Resolution of Kata 5287e858c6b5a9678200083c at https://www.codewars.com

---
+++ src/main/Kyu_6/DoesMyNumberLookBigInThis.java
@@ -0,0 +1,32 @@
+package main.Kyu_6;
+
+public class DoesMyNumberLookBigInThis {
+
+    public static void main(String[] args) {
+        System.out.println(solution(152));
+        System.out.println(solution(153));
+        System.out.println(solution(18234));
+    }
+
+    public static boolean solution(int givenNumber) {
+        if(givenNumber >=0 && givenNumber < 10){ return true; }
+
+        char[] nums = String.valueOf(givenNumber).toCharArray();
+
+        int singleNumFromNumsArray = 0;
+        int sumNumber = 0;
+        int tempNumber = 0;
+
+        for (char num : nums) {
+            singleNumFromNumsArray = Integer.valueOf(String.valueOf(num));
+            tempNumber = singleNumFromNumsArray;
+
+            for(int i = 1;i < nums.length;i++) {
+                tempNumber = tempNumber * singleNumFromNumsArray;
+            }
+            sumNumber += tempNumber;
+        }
+
+        return sumNumber == givenNumber;
+    }
+}
